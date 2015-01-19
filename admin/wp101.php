07a3e53c64ea02de3589aa0985a816ddf9b9768d
imforza <imforza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-19 22:37:49 +0000

    Added a few IDX Broker Youtube Videos
    git-svn-id: https://plugins.svn.wordpress.org/imforza-news/trunk@1071360 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/wp101.php
+++ admin/wp101.php
@@ -5,3 +5,37 @@
 update_option( 'wp101_hidden_topics', array('14', '17', '18', '19', '20', 'wpseo.9', 'wpseo.10', 'wpseo.11', 'wpseo.12', 'wpseo.13', 'wpseo.14', 'wpseo.15', 'wpseo.16','wpseo.17') );
 
 
+
+
+add_filter( 'wp101_get_custom_help_topics', function( $custom_videos ) {
+
+    $custom_videos['idxbroker.1'] = array(
+        'id'      => 'idxbroker.1',
+        'title'   => 'IDX Broker: Getting Started',
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/Gm-R4l23tp8?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+    );
+
+    $custom_videos['idxbroker.2'] = array(
+        'id'      => 'idxbroker.2',
+        'title'   => 'IDX Broker: Creating/Managing Pages',
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/cHg-Gx5P_Uo?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+    );
+
+    $custom_videos['idxbroker.3'] = array(
+        'id'      => 'idxbroker.3',
+        'title'   => 'IDX Broker: Social and Facebook',
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/GvvsVgp_PLI?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+    );
+
+    $custom_videos['idxbroker.4'] = array(
+        'id'      => 'idxbroker.4',
+        'title'   => 'IDX Broker: Lead Management',
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/jU5Z0z_GOHA?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+    );
+
+    return $custom_videos;
+} );
+
+
+
+
