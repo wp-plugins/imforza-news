d70bc97765348a47de9ccea47de565a10fce34ee
imforza <imforza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-19 22:41:22 +0000

    updated youtube embeds
    git-svn-id: https://plugins.svn.wordpress.org/imforza-news/trunk@1071361 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/wp101.php
+++ admin/wp101.php
@@ -12,25 +12,25 @@ add_filter( 'wp101_get_custom_help_topics', function( $custom_videos ) {
     $custom_videos['idxbroker.1'] = array(
         'id'      => 'idxbroker.1',
         'title'   => 'IDX Broker: Getting Started',
-        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/Gm-R4l23tp8?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/Gm-R4l23tp8?rel=0&showinfo=0&controls=1&vq=hd1080&modestbranding=1" frameborder="0" allowfullscreen></iframe>'
     );
 
     $custom_videos['idxbroker.2'] = array(
         'id'      => 'idxbroker.2',
         'title'   => 'IDX Broker: Creating/Managing Pages',
-        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/cHg-Gx5P_Uo?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/cHg-Gx5P_Uo?rel=0&showinfo=0&controls=1&vq=hd1080&modestbranding=1" frameborder="0" allowfullscreen></iframe>'
     );
 
     $custom_videos['idxbroker.3'] = array(
         'id'      => 'idxbroker.3',
         'title'   => 'IDX Broker: Social and Facebook',
-        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/GvvsVgp_PLI?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/GvvsVgp_PLI?rel=0&showinfo=0&controls=1&vq=hd1080&modestbranding=1" frameborder="0" allowfullscreen></iframe>'
     );
 
     $custom_videos['idxbroker.4'] = array(
         'id'      => 'idxbroker.4',
         'title'   => 'IDX Broker: Lead Management',
-        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/jU5Z0z_GOHA?rel=0&showinfo=0&controls=1&vq=hd1080" frameborder="0" allowfullscreen></iframe>'
+        'content' => '<iframe width="560" height="315" src="//www.youtube.com/embed/jU5Z0z_GOHA?rel=0&showinfo=0&controls=1&vq=hd1080&modestbranding=1" frameborder="0" allowfullscreen></iframe>'
     );
 
     return $custom_videos;
